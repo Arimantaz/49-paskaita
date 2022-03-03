296b93ba9b10a4a20ce274683f73fcff7f6b1409
MARIUS MOCKUS <mariusmockus93@gmail.com>
2022-03-03 21:10:25 +0200

    Nebaigta SelfMessage

--- events.js
+++ events.js
@@ -15,13 +15,21 @@ function createEl() {
 }
 
 function selfMessage(){
+    const main = document.querySelector('.chat-container');
     const art = document.createElement('article');
     const sectionImage = document.createElement('section');
-    const sectionBuble = document.createElement('section');
+    const sectionBubble = document.createElement('section');
+    main.appendChild(art);
     art.classList.add('message', 'self');
     sectionImage.classList.add('image');
+    sectionBubble.classList.add('chat-bubble');
+    art.appendChild(sectionImage);
+    art.appendChild(sectionBubble);
 
 }
+
+selfMessage()
+
 
 
 // lintjs
