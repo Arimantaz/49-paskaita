ecda8e041a6f596d9d4ad9624f744b42485de78d
Raimondas Knystautas <email@com.com>
2022-03-03 19:02:52 +0200

    Reaguoja mygtukas

--- events.js
+++ events.js
@@ -1,1 +1,8 @@
 const text = document.querySelector('textarea');
+const button = document.querySelector('button');
+
+button.addEventListener('click', pokalbis);
+
+function pokalbis() {
+    console.log('test');
+}
